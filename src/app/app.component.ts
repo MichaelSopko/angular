c8df6c5aa4f047d0e52c603170840425bcb27fd9
wolfman <andriy.vovk@thinkmobiles.com>
2017-08-31 16:14:09 +0300

    Add angular animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,44 @@
 import {Component} from '@angular/core';
 import {AppService} from './core/app.service';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({
+        transform: 'translate3d(0, 0, 0)'
+      })),
+      state('out', style({
+        transform: 'translate3d(-100%, 0, 0)'
+      })),
+      transition('in => out', animate('270ms ease-in-out')),
+      transition('out => in', animate('270ms ease-in-out'))
+    ]),
+  ]
 })
 export class AppComponent {
   sideBarIsVisible = true;
   isDashboardOpened = false;
+  menuState: string = 'in';
+
+  toggleMenu() {
+    // 1-line if statement that toggles the value:
+    this.menuState = this.menuState === 'out' ? 'in' : 'out';
+  }
+
+  animationDone() {
+
+   console.log(' Animation Done');
+  }
 
   constructor(private _appService: AppService) {
   }
